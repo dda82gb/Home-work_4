eba705944c6c3743b31a01f9ab5396c2f0f473c7
Davran_Djumanov <dda_82@mail.ru>
2023-02-08 09:08:36 +0600

    tesk_27

---
+++ task_27/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 27: Напишите программу, которая принимает 
+// на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+Console.Write("Введите число:");
+int x = int.Parse(Console.ReadLine());
+int sum = 0;
+
+while (x > 0)
+{
+int y = x % 10;
+x = x / 10;
+sum = sum + y;
+}
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Сумма всех цифр в числе = " + sum);
