3e7ff01a2c1bdfe599cfc58823f060cf612115ad
Davran_Djumanov <dda_82@mail.ru>
2023-02-08 08:37:29 +0600

    tesk_25

---
+++ task_25/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два 
+// числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Clear();
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("введите число A");
+int numA = int.Parse(Console.ReadLine()!);
+Console.WriteLine("введите число B");
+int numB = int.Parse(Console.ReadLine()!);
+int NatStep = numA;
+
+for (int i = 1; i < numB; i++)
+{
+NatStep = NatStep * numA;
+}
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("A в степени B = " + NatStep);
